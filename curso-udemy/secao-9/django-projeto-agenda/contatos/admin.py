904b7a0584fea0b779071d11852f8488bcdefebb
Ig0or <igorprovensi@gmail.com>
2021-09-28 14:48:06 -0300

    editado area administrativa

--- curso-udemy/secao-9/django-projeto-agenda/contatos/admin.py
+++ curso-udemy/secao-9/django-projeto-agenda/contatos/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
-# Register your models here.
+
+# editar area administrativa e caracteristicas da categoria contatos
+class ContatoAdmin(admin.ModelAdmin):
+    # deixar os campos visiveis
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    # deixar os campos clicaveis
+    list_display_links = ('id', 'nome', 'sobrenome')
+    # opcao de filtrar
+    list_filter = ('nome', 'sobrenome')
+    # buscar campos
+    search_fields = ('nome', 'sobrenome', 'telefone')
+    list_per_page = 10
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
