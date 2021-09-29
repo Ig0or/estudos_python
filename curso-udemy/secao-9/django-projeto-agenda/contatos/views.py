eb78e24b175249dfeada37e36bfa5da8c5a74d4f
Ig0or <igorprovensi@gmail.com>
2021-09-29 14:39:32 -0300

    ordenação

--- curso-udemy/secao-9/django-projeto-agenda/contatos/views.py
+++ curso-udemy/secao-9/django-projeto-agenda/contatos/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
 
 
 def index(request):
-    contatos = Contato.objects.all()
-    paginator = Paginator(contatos, 1)
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
+    paginator = Paginator(contatos, 2)
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
     return render(request, 'contatos/index.html', {
@@ -15,6 +18,9 @@ def index(request):
 
 def ver_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
