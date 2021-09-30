966984151b1540236a051e3490a226af2a5e0b9f
Ig0or <igorprovensi@gmail.com>
2021-09-30 16:01:03 -0300

    inicio cadastro de usuarios

---
+++ curso-udemy/secao-9/django-projeto-agenda/accounts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.contrib import messages
+
+# Create your views here.
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return render(request, 'accounts/logout.html')
+
+
+def cadastro(request):
+    if request.method != 'POST':
+        return render(request, 'accounts/cadastro.html')
+
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    email = request.POST.get('email')
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+    senha2 = request.POST.get('senha2')
+
+    if not nome or not sobrenome or not email or not usuario or not senha or not senha2:
+        messages.error(request, 'Nenhum campo pode ficar vazio.')
+
+    return render(request, 'accounts/cadastro.html')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
